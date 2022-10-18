4e0724143a55f741b3bfb10be7ffe23ae1d65ae2
saranell <saranell92@gmail.com>
2022-10-18 15:31:33 -0500

    15.3.5 applying validation attributes

--- CodingEvents/ViewModels/AddEventViewModel.cs
+++ CodingEvents/ViewModels/AddEventViewModel.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace CodingEvents.ViewModels
 {
     public class AddEventViewModel
     {
+        [Required(ErrorMessage ="Your event needs a name!")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage ="Name must be between 3 and 50 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage ="Your event needs a description!")]
+        [StringLength(500, ErrorMessage ="Description is limited to 500 characters.")]
         public string Description { get; set; }
-        
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+
     }
 }
 
